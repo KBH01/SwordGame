b91dd3e10b0ab1ef08cc349ddd70c9af26523216
Kai Herbst <kbherbst16@gmail.com>
2023-07-27 17:11:16 -0700

    Added Basic Sword Controls
    player can control the sword based on where their mouse is on the screen

--- SwordGame/Assets/Scripts/SwordScript.cs
+++ SwordGame/Assets/Scripts/SwordScript.cs
@@ -1,18 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SwordScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    private static float resolutionRatio = 0;
+    
+    [SerializeField] float sensitivity = 0;
+    
+    [SerializeField] private Transform idle;
+    [SerializeField] private Transform left;
+    [SerializeField] private Transform right;
+    [SerializeField] private Transform top;
+    [SerializeField] private Transform bottom;
+    
     void Update()
     {
+        resolutionRatio = (float)Screen.height / (float)Screen.width;
+        float mouseX = Input.mousePosition.x;
+        float mouseY = Input.mousePosition.y;
         
+        Debug.Log(Screen.width + ", " + Screen.height + ", " + resolutionRatio);
+
+        if (Vector2.Distance(new Vector2(Screen.width * 0.5f, Screen.height * 0.5f), new Vector2(mouseX, mouseY)) > sensitivity)
+        {
+
+            if (mouseY > Line1(mouseX) && mouseY > Line2(mouseX))
+            {
+                transform.SetPositionAndRotation(top.position, top.rotation);
+            }
+
+            else if (mouseY < Line1(mouseX) && mouseY < Line2(mouseX))
+            {
+                transform.SetPositionAndRotation(bottom.position, bottom.rotation);
+            }
+
+            if (mouseY > Line1(mouseX) && mouseY < Line2(mouseX))
+            {
+                transform.SetPositionAndRotation(left.position, left.rotation);
+            }
+
+            else if (mouseY < Line1(mouseX) && mouseY > Line2(mouseX))
+            {
+                transform.SetPositionAndRotation(right.position, right.rotation);
+            }
+        }
+
+        else
+        {
+            transform.SetPositionAndRotation(idle.position, idle.rotation);
+        }
+
+    }
+
+    //these are the lines that split up the screen into areas for the mouse
+    float Line1(float x)
+    {
+        float y = 0;
+
+        y = x * resolutionRatio;
+
+        return y;
+    }
+    
+    float Line2(float x)
+    {
+        float y = 0;
+
+        y = x * -resolutionRatio + (float)Screen.height;
+
+        return y;
     }
 }
